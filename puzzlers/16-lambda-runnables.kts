e2bc39ac0d9d878b9cd188c8beefb32a0b39d829
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-10-01 13:33:56 +0200

    misc

--- puzzlers/16-lambda-runnables.kts
+++ puzzlers/16-lambda-runnables.kts
@@ -1,19 +1,23 @@
 
 fun test() {
+
     val run: () -> Unit = {
-        println("Hello World!")
+        println("Run run run!")
     }
 
     object : Runnable {
         override fun run() = run()
     }.run()
+
 }
+test()
 
 /*
+A) Run run run!
 
-A) Hello World
-
-B) Doesn't compile
+B) doesn't compile
 
 C) StackOverflowError
- */
+
+D) none of the above
+*/
