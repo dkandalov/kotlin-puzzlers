900335681aedac2a806c740f5745bf85efcb1f8f
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2018-04-23 19:37:39 +0100

    added puzzler

---
+++ puzzlers/x-const-null.kt
@@ -0,0 +1,16 @@
+
+const val osName: String? = System.getProperty("os.name")
+
+fun main(args: Array<String>) {
+    println(osName)
+}
+
+/*
+A) 
+
+B) null
+
+C) Mac OS X
+
+D) Doesn't compile
+*/
